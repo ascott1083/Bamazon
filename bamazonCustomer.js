da324df81ebca5c1f2118583445ca472eda4ad13
AJ <ascott1083@yahoo.com>
2019-10-19 16:04:24 -0500

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,77 +1,86 @@
-const inquirer  = require('inquirer');
-const mysql = require('mysql');
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+
+let port = 3306;
 
 let connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'bamazon'
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "bamazon_DB"
 });
 
-connection.connect();
+connection.connect(function(err){
+    console.log('we try to using this function');
+    
+  if (err) throw err;
+  console.log("Connection Sucessful!")
+  makeTable();
+})
+ 
+let makeTable = function() {
+connection.query('SELECT * FROM products', function(err, res){
+    if(err) throw err;
+    for(let i = 0; i<res.length;i++){
+        console.log("Items:") 
+        console.log("ID: " + res[i].item_id + " || " + "Product: " + res[i].product_name + " || " + "Department: " + res[i].department_name + " || " + "Price: " + res[i].price + " || " + "QTY: " + res[i].stock_quantity);
+        console.log('----------------------------------------------------------------------------------')
+    }
+    promptCustomer(res);
 
-start()
+  })
+}
 
-function start (){
-    connection.query('SELECT * FROM products', function (error, res) {
-        if (error) throw error;
-        // console.log(res);
-        res.forEach(row => {
-            console.log(`Id: ${row.item_id} Name: ${row.product_name} Price: ${row.price}\n` )
-        });
-        askQuestions()
-
-        // connection.end()
+let promptCustomer = function(res) {
+     inquirer.prompt([{
+        type: "input", 
+        name: "choice",
+        message: "Type desired item here: ",
+      }])
+      .then(function(answer){
+        let correct = false;
+        if(answer.choice.toUpperCase()=="Q") {
+          process.exit()
+        }
+        
+    for(let i = 0; i<res.length;i++) {
+        if (res[i].product_name==answer.choice){
+         correct = true;
+        let product= answer.choice
+        let id=i
+        inquirer.prompt({
+        type: "input",
+        name: "quant",
+        message: "How many would you like to buy?",
+              
+    validate: function(value){
+        if(isNaN(value)==false) {
+        return true;
+       } else {
+        return false;
+        }
+    }
+     })
+     
+     .then(function(answer){
+        if((res[id].stock_quantity-answer.quant)>0){
+            connection.query("UPDATE products SET stock_quantity='" + (res[id].stock_quantity-answer.quant)+"' WHERE product_name='"+product+"'", function(err, res2){
+                console.log("Purchased!")
+                makeTable()
+     })
+              
+        } else {
+            console.log("Not Valid Selection!")
+            promptCustomer(res)
+        }
     })
 }
-
-function askQuestions() {
-    inquirer.prompt([
-        {
-            message: "Please type in the product id you would like to order.",
-            type: "input",
-            name: "prodId"
-        },
-        {
-            message: "how many of this item would you like to purchase",
-            type: "input",
-            name: "prodQty"
+}
+        if (i==res.length && correct==false){
+          console.log("Not a valid selection!")
+          promptCustomer(res);
         }
-    ]).then(function (ans) {
-        var prodId = ans.prodId;
-        var prodQty = ans.prodQty;
-        // var allProd = getAllProd();
-        withdrawProd(prodId, prodQty)
-    });
-}
-
-function withdrawProd(prodId, prodQty) {
-  connection.query('SELECT * FROM products', function (error, res) {
-    if (error) throw error;
-    var prod;
-    // console.log(res);
-    for(var i = 0; i < res.length; i++){
-      if(res[i].item_id == prodId){
-        prod = res[i]
-      }
+      })
     }
-    console.log(prod, "prod was found")
-      if(prod.stock_quantity >= prodQty){
-        orderComplete(prod, prodId, prodQty)
-        connection.end()
-      }else{
-        console.log("sorry the order has been cancled, there was insuffecent stock of this purchase")
-        connection.end()
-      }
-  })
-};
-function orderComplete (prodObj, prodId, prodQty) {
-  var newQuantity = prodObj.stock_quantity - prodQty;
-  var productSales = prodObj.price * prodQty;
-  var queryOne = "UPDATE products SET stock_quantity = ? where ?";
-  var queryTwo = "UPDATE products SET product_sales = ? where ?";
-  connection.query(queryOne,[newQuantity, {item_id: prodId}], function (error, res) {
-  })
-  connection.query(queryTwo, [productSales, { item_id: prodId }], function (error, res) {
-  })
-}
+      
