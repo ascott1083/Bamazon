a3e4384f24da171ad3317482855691c09cae62c7
AJ <ascott1083@yahoo.com>
2020-01-17 19:18:06 -0600

    d

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,9 +2,7 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-  
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "",
   database: "bamazon_DB"
